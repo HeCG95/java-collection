f3c9bcc261856e79d881780c9d0395a134617b27
HeCG95 <jacktanger@163.com>
2019-10-30 01:15:41 +0800

    [A] add demos for RocketMQ message repeats problem. 191030

---
+++ java-rocketmq/src/main/java/cn/rumoss/study/rocketmq/model/Consumer2.java
@@ -0,0 +1,53 @@
+package cn.rumoss.study.rocketmq.model;
+
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
+import com.alibaba.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public class Consumer2 {
+
+    public static void main(String[] args) throws MQClientException {
+
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("message_consumer");
+        consumer.setNamesrvAddr("192.168.2.182:9876;192.168.2.183:9876");
+
+        /**
+         * 设置Consumer第一次启动是从队列头部开始消费还是队列尾部开始消费<br>
+         * 如果非第一次启动，那么按照上次消费的位置继续消费
+         */
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+
+        consumer.subscribe("Topic1", "Tag1 || Tag2 || Tag3");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            //@Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                                                            ConsumeConcurrentlyContext context) {
+                try {
+
+                    for(MessageExt msg : msgs){
+                        String msgBody = new String(msg.getBody(), "UTF-8");
+                        System.out.println("Receive message: " + msgBody + msg);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumer.start();
+
+        System.out.println("Consumer Started.");
+
+    }
+
+}
---
+++ java-rocketmq/src/main/java/cn/rumoss/study/rocketmq/model/Producer.java
@@ -0,0 +1,35 @@
+package cn.rumoss.study.rocketmq.model;
+
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.common.message.Message;
+
+public class Producer {
+
+    public static void main(String[] args) throws MQClientException,InterruptedException {
+
+        DefaultMQProducer producer = new DefaultMQProducer("message_producer");
+        producer.setNamesrvAddr("192.168.2.182:9876;192.168.2.183:9876");
+
+        producer.start();
+
+//        for(int i=0;i<100;i++){
+        for(int i=0;i<1;i++){
+            try {
+                Message message = new Message("Topic1",
+                        "Tag1",
+                        ("Message Content " + i).getBytes());
+                SendResult sendResult = producer.send(message);
+                System.out.println(sendResult);
+            }catch (Exception e){
+                e.printStackTrace();
+                Thread.sleep(1000);
+            }
+        }
+
+        producer.shutdown();
+
+    }
+
+}
